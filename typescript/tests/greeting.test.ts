6cba0215e76e6b575abde8eaf62fe8a7a66bf94a
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-07-11 11:10:22 +0200

    setup typescript project

---
+++ typescript/tests/greeting.test.ts
@@ -0,0 +1,7 @@
+import { greeting } from "../src/greeting";
+
+describe('greeting', () => {
+  it('fixme: should return a greet', async () => {
+    expect(greeting()).toBe("Hello World");
+  });
+});
